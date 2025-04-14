09378fd99edbbac23d038ac9191dcfb1be70f835
Jason Shi <jshigoodies@gmail.com>
2025-04-14 00:04:35 -0500

    player join event

--- src/main/java/jshi/Jason/Main.java
+++ src/main/java/jshi/Jason/Main.java
@@ -10,7 +10,7 @@ import javax.security.auth.login.LoginException;
 
 public class Main extends JavaPlugin {
     private Bot bot;
-    private final String token = "MTM0NDIwMzIxNDI2NTkwOTI1MQ.GI7tqu.fB0h4EBYYjH5GgLXfE8hwr1c-tlTimKVqB5br4";
+    private final String token = "";
     @Override
     public void onEnable() {
         this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[DiscordXSpigot] Started");
